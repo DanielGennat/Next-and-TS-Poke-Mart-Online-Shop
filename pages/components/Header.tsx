1bdef6a22ae8ce607682525e1be5a8e324647d7f
X4 <daniel.gennat@freenet.de>
2023-01-26 13:30:27 +0100

    add Header

---
+++ pages/components/Header.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export default function Header() {
+    return (
+        <H1>Poké Mart Online Shop</H1>
+    );
+}
+
+const H1 = styled.h1`
+    color: #FFCE00;
+    /* text-shadow: 3px 0px #3264AF, -3px 0px #3264AF, 0px 3px #3264AF, 0px -3px #3264AF,
+                 3px 3px #3264AF, -3px -3px #3264AF, -3px 3px #3264AF, 3px -3px #3264AF; */
+    text-shadow: 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF,
+                 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF,
+                 0px 0px 3px #3264AF, 0px 0px 3px #3264AF, 0px 0px 3px #3264AF, 0px 0px 3px #3264AF,
+                 0px 0px 2px #3264AF, 0px 0px 2px #3264AF, 0px 0px 2px #3264AF, 0px 0px 2px #3264AF,
+                 0px 0px 1px #3264AF, 0px 0px 1px #3264AF, 0px 0px 1px #3264AF, 0px 0px 1px #3264AF,
+                 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF,
+                 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF,
+                 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF,
+                 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF,
+                 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF,
+                 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF,
+                 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF, 0px 0px 5px #3264AF,
+                 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF, 0px 0px 4px #3264AF;
+    text-align: center;
+`;
